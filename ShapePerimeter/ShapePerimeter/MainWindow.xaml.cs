f23a5bb740d700de971ece8025f176f5add18acb
Tiran <tirans3777@gmail.com>
2020-01-15 12:21:10 +0400

    add view

--- ShapePerimeter/ShapePerimeter/MainWindow.xaml.cs
+++ ShapePerimeter/ShapePerimeter/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ShapePerimeter.Data.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ShapePerimeter
 {
@@ -20,9 +8,11 @@ namespace ShapePerimeter
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new ShapeViewModel();
         }
     }
 }
---
+++ ShapePerimeter/ShapePerimeter/View/ShapeParameterWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using ShapePerimeter.Data.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ShapePerimeter.View
+{
+    /// <summary>
+    /// Interaction logic for ShapeParameterWindow.xaml
+    /// </summary>
+    public partial class ShapeParameterWindow : Window
+    {
+       
+        public ShapeParameterWindow(IShape shape)
+        {
+            InitializeComponent();
+            DataContext = new ShapeParametersViewModel(shape);
+            var control = (ContentControl)this.FindName(shape.Type.ToString());
+            control.Visibility = Visibility.Visible;
+        }
+
+        private void OnApprove_Click(object sender, RoutedEventArgs e) { this.DialogResult = true; Close(); }
+        private void OnCancel_Click(object sender, RoutedEventArgs e) { this.DialogResult = false;Close(); }
+    }
+}
